76ab2f37952619bc0f883547d6da4beba5268c95
Krit Hnoorun <krithnoorun@gmail.com>
2025-06-19 12:19:34 +0700

    redirect home page

--- utils/config.ts
+++ utils/config.ts
@@ -1,2 +1,3 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://automate-internship-screening-backend.onrender.com";
